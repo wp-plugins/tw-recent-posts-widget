fe2f84ab282bc5eaddf4b104e29e60c8477b6030
tweetysha <tweetysha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-22 23:01:58 +0000

    Added new features.
    git-svn-id: https://plugins.svn.wordpress.org/tw-recent-posts-widget/trunk@657145 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tw-recent-posts-widget.php
+++ tw-recent-posts-widget.php
@@ -5,7 +5,7 @@ Plugin URI: http://vuckovic.biz/wordpress-plugins/tw-recent-posts-widget
 Description: TW Recent Posts Widget is advanced version of the WordPress Recent Posts widget allowing increased customization to display recent posts from category you define.
 Author: Igor Vučković
 Author URI: http://vuckovic.biz
-Version: 1.0.2
+Version: 1.0.3
 */
 
 //	Set the wp-content and plugin urls/paths
@@ -21,7 +21,7 @@ if (! defined ( 'WP_PLUGIN_DIR' ))
 class TW_Recent_Posts extends WP_Widget {
 	
 	//	@var string (The plugin version)		
-	var $version = '1.0.2';
+	var $version = '1.0.3';
 	//	@var string $localizationDomain (Domain used for localization)
 	var $localizationDomain = 'tw-recent-posts';
 	//	@var string $pluginurl (The url to this plugin)
@@ -116,6 +116,7 @@ class TW_Recent_Posts extends WP_Widget {
 		$height = apply_filters ( 'height', isset ( $instance ['height'] ) && is_numeric ( $instance ['height'] ) ? $instance ['height'] : '60' );
 		$length = apply_filters ( 'length', isset ( $instance ['length'] ) && is_numeric ( $instance ['length'] ) ? $instance ['length'] : '100' );
 		$show_post_title = apply_filters ( 'show_post_title', isset ( $instance ['show_post_title'] ) ? ( bool ) $instance ['show_post_title'] : false );
+		$show_post_time = apply_filters ( 'show_post_time', isset ( $instance ['show_post_time'] ) ? ( bool ) $instance ['show_post_time'] : false );
 		$show_post_thumb = apply_filters ( 'show_post_thumb', isset ( $instance ['show_post_thumb'] ) ? ( bool ) $instance ['show_post_thumb'] : ( bool ) false );
 		$show_post_excerpt = apply_filters ( 'show_post_excerpt', isset ( $instance ['show_post_excerpt'] ) ? ( bool ) $instance ['show_post_excerpt'] : false );
 		
@@ -135,18 +136,22 @@ while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 	title="<?php the_title_attribute() ?>"><?php the_title() ?></a></h4>
 	<?php } ?>
 
+	<?php if ($show_post_time) { ?>
+		<div class="post-time">
+			<?php the_time ( get_option ( 'date_format' ) ); ?>
+		</div>
+	<?php } ?>
+	
 	<?php if ($show_post_thumb && has_post_thumbnail()) { ?>
 		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array($width,$height), array('title' => '', 'class' => 'alignleft')); ?></a>
 	<?php } ?>
 	
 	<?php if ($show_post_excerpt) { ?>
 		<div class="excerpt">
-			<?php echo $this->truncate_post($length) . ($moretext != '') ? ' <a href="' . get_permalink () . '" class="read-more">' . $moretext . '</a>' : '';
-				?>
+			<?php echo $this->truncate_post($length) . ($moretext != '') ? ' <a href="' . get_permalink () . '" class="read-more">' . $moretext . '</a>' : ''; ?>
 		</div>
-	<?php
-			}
-			?>
+	<?php } ?>
+	
 		<div class="clear"></div>
 	</div>
 <?php
@@ -174,6 +179,7 @@ wp_reset_postdata();
 		$height = isset ( $instance ['height'] ) && is_numeric ( $instance ['height'] ) ? $instance ['height'] : '60';
 		$length = isset ( $instance ['length'] ) && is_numeric ( $instance ['length'] ) ? $instance ['length'] : '100';
 		$show_post_title = isset ( $instance ['show_post_title'] ) ? ( bool ) $instance ['show_post_title'] : false;
+		$show_post_time = isset ( $instance ['show_post_time'] ) ? ( bool ) $instance ['show_post_time'] : false;
 		$show_post_thumb = isset ( $instance ['show_post_thumb'] ) ? ( bool ) $instance ['show_post_thumb'] : false;
 		$show_post_excerpt = isset ( $instance ['show_post_excerpt'] ) ? ( bool ) $instance ['show_post_excerpt'] : false;
 ?>
@@ -187,6 +193,7 @@ wp_reset_postdata();
 	id="<?php echo $this->get_field_id('category'); ?>"
 	name="<?php echo $this->get_field_name('category'); ?>">
 	<?php 
+	echo '<option value="0" ' .( '0' == $category ? 'selected="selected"' : '' ). '>'. __('All categories', $this->localizationDomain).'</option>';
 	$cats = get_categories(array('hide_empty' => 0, 'name' => 'category', 'hierarchical' => true));
 	foreach ($cats as $cat) {
 		echo '<option value="' . $cat->term_id . '" ' .( $cat->term_id == $category ? 'selected="selected"' : '' ). '>' . $cat->name . '</option>';
@@ -227,13 +234,19 @@ wp_reset_postdata();
 <p><input id="<?php echo $this->get_field_id('show_post_title'); ?>"
 	name="<?php echo $this->get_field_name('show_post_title'); ?>"
 	type="checkbox" <?php checked($show_post_title); ?> /> <label
-	for="<?php echo $this->get_field_id('show_post_title'); ?>"><?php _e('Show post title:', $this->localizationDomain); ?></label>
+	for="<?php echo $this->get_field_id('show_post_title'); ?>"><?php _e('Show post title', $this->localizationDomain); ?></label>
+</p>
+
+<p><input id="<?php echo $this->get_field_id('show_post_time'); ?>"
+	name="<?php echo $this->get_field_name('show_post_time'); ?>"
+	type="checkbox" <?php checked($show_post_time); ?> /> <label
+	for="<?php echo $this->get_field_id('show_post_time'); ?>"><?php _e('Show post time', $this->localizationDomain); ?></label>
 </p>
 
 <p><input id="<?php echo $this->get_field_id('show_post_thumb'); ?>"
 	name="<?php echo $this->get_field_name('show_post_thumb'); ?>"
 	type="checkbox" <?php checked($show_post_thumb); ?> /> <label
-	for="<?php echo $this->get_field_id('show_post_thumb'); ?>"><?php _e('Show post thumb:', $this->localizationDomain); ?></label><br />
+	for="<?php echo $this->get_field_id('show_post_thumb'); ?>"><?php _e('Show post thumb', $this->localizationDomain); ?></label><br />
 <small><?php _e('Thumbnail size (W-H):', $this->localizationDomain); ?></small>
 <input type="text" size="3"
 	name="<?php echo $this->get_field_name('width'); ?>"
@@ -244,7 +257,7 @@ wp_reset_postdata();
 <p><input id="<?php echo $this->get_field_id('show_post_excerpt'); ?>"
 	name="<?php echo $this->get_field_name('show_post_excerpt'); ?>"
 	type="checkbox" <?php checked($show_post_excerpt); ?> /> <label
-	for="<?php echo $this->get_field_id('show_post_excerpt'); ?>"><?php _e('Show post excerpt:', $this->localizationDomain); ?></label><br />
+	for="<?php echo $this->get_field_id('show_post_excerpt'); ?>"><?php _e('Show post excerpt', $this->localizationDomain); ?></label><br />
 <small><?php _e('Post excerpt length (characters)', $this->localizationDomain); ?></small>
 <input id="<?php echo $this->get_field_id('length'); ?>"
 	name="<?php echo $this->get_field_name('length'); ?>" type="text"
